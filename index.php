be728c6afa369df45222f6925cb894ae2a751d26
LoganPaczek <paczek.logan05@gmail.com>
2025-04-22 14:01:57 +0200

    Create Project Structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include_once('./src/views/components/Head.php') ?>
+    <title>Gestion des stocks</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
